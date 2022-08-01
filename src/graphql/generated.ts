6063c30d7ff215b611453aa99ade02cf9cc9a03b
Younglong Kim <withel1013@gmail.com>
2022-08-02 07:06:43 +0900

    GraphQL Code Generator

---
+++ src/graphql/generated.ts
@@ -0,0 +1,582 @@
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  DateTime: any;
+};
+
+export type AllCategoriesOutput = {
+  __typename?: 'AllCategoriesOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  categories?: Maybe<Array<Category>>;
+};
+
+export type AllRestaurantsInput = {
+  page?: InputMaybe<Scalars['Int']>;
+};
+
+export type AllRestaurantsOutput = {
+  __typename?: 'AllRestaurantsOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  restaurants?: Maybe<Array<Restaurant>>;
+  totalPages?: Maybe<Scalars['Int']>;
+  totalResults?: Maybe<Scalars['Int']>;
+};
+
+export type Category = {
+  __typename?: 'Category';
+  countRestaurants: Scalars['Int'];
+  coverImg?: Maybe<Scalars['String']>;
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Float'];
+  name: Scalars['String'];
+  restaurants?: Maybe<Array<Restaurant>>;
+  slug: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type CategoryInput = {
+  page?: InputMaybe<Scalars['Int']>;
+  slug: Scalars['String'];
+};
+
+export type CategoryOutput = {
+  __typename?: 'CategoryOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  category?: Maybe<Category>;
+  restaurants?: Maybe<Array<Restaurant>>;
+  totalPages?: Maybe<Scalars['Int']>;
+  totalResults?: Maybe<Scalars['Int']>;
+};
+
+export type CreateAccountInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+  role: UserRole;
+};
+
+export type CreateAccountOutput = {
+  __typename?: 'CreateAccountOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type CreateDishInput = {
+  description: Scalars['String'];
+  name: Scalars['String'];
+  options?: InputMaybe<Array<DishOptionInputType>>;
+  price: Scalars['Int'];
+  restaurantId: Scalars['Int'];
+};
+
+export type CreateDishOutput = {
+  __typename?: 'CreateDishOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type CreateOrderInput = {
+  items: Array<CreateOrderItemInput>;
+  restaurantId: Scalars['Int'];
+};
+
+export type CreateOrderItemInput = {
+  dishId: Scalars['Int'];
+  options?: InputMaybe<Array<OrderItemOptionInputType>>;
+};
+
+export type CreateOrderOutput = {
+  __typename?: 'CreateOrderOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type CreatePaymentInput = {
+  restaurantId: Scalars['Int'];
+  transactionId: Scalars['String'];
+};
+
+export type CreatePaymentOutput = {
+  __typename?: 'CreatePaymentOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type CreateRestaurantInput = {
+  address: Scalars['String'];
+  categoryName: Scalars['String'];
+  coverImg: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type CreateRestaurantOutput = {
+  __typename?: 'CreateRestaurantOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type DeleteDishInput = {
+  dishId: Scalars['Int'];
+};
+
+export type DeleteDishOutput = {
+  __typename?: 'DeleteDishOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type DeleteRestaurantInput = {
+  restaurantId: Scalars['Float'];
+};
+
+export type DeleteRestaurantOutput = {
+  __typename?: 'DeleteRestaurantOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type Dish = {
+  __typename?: 'Dish';
+  createdAt: Scalars['DateTime'];
+  description: Scalars['String'];
+  id: Scalars['Float'];
+  name: Scalars['String'];
+  options?: Maybe<Array<DishOption>>;
+  photo?: Maybe<Scalars['String']>;
+  price: Scalars['Int'];
+  restaurant: Restaurant;
+  updatedAt: Scalars['DateTime'];
+};
+
+export type DishChoice = {
+  __typename?: 'DishChoice';
+  extra?: Maybe<Scalars['Int']>;
+  name: Scalars['String'];
+};
+
+export type DishChoiceInputType = {
+  extra?: InputMaybe<Scalars['Int']>;
+  name: Scalars['String'];
+};
+
+export type DishOption = {
+  __typename?: 'DishOption';
+  choices?: Maybe<Array<DishChoice>>;
+  extra?: Maybe<Scalars['Int']>;
+  name: Scalars['String'];
+};
+
+export type DishOptionInputType = {
+  choices?: InputMaybe<Array<DishChoiceInputType>>;
+  extra?: InputMaybe<Scalars['Int']>;
+  name: Scalars['String'];
+};
+
+export type EditDishInput = {
+  description?: InputMaybe<Scalars['String']>;
+  dishId: Scalars['Int'];
+  name?: InputMaybe<Scalars['String']>;
+  options?: InputMaybe<Array<DishOptionInputType>>;
+  price?: InputMaybe<Scalars['Int']>;
+};
+
+export type EditDishOutput = {
+  __typename?: 'EditDishOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type EditOrderInput = {
+  id: Scalars['Float'];
+  status: OrderStatus;
+};
+
+export type EditOrderOutput = {
+  __typename?: 'EditOrderOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type EditProfileInput = {
+  email?: InputMaybe<Scalars['String']>;
+  password?: InputMaybe<Scalars['String']>;
+};
+
+export type EditProfileOutput = {
+  __typename?: 'EditProfileOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type EditRestaurantInput = {
+  address?: InputMaybe<Scalars['String']>;
+  categoryName?: InputMaybe<Scalars['String']>;
+  coverImg?: InputMaybe<Scalars['String']>;
+  name?: InputMaybe<Scalars['String']>;
+  restaurantId: Scalars['Float'];
+};
+
+export type EditRestaurantOutput = {
+  __typename?: 'EditRestaurantOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type GetOrderInput = {
+  id: Scalars['Float'];
+};
+
+export type GetOrderOutput = {
+  __typename?: 'GetOrderOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  order?: Maybe<Order>;
+};
+
+export type GetOrdersInput = {
+  status?: InputMaybe<OrderStatus>;
+};
+
+export type GetOrdersOutput = {
+  __typename?: 'GetOrdersOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  orders?: Maybe<Array<Order>>;
+};
+
+export type GetPaymentsOutput = {
+  __typename?: 'GetPaymentsOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  payments?: Maybe<Array<Payment>>;
+};
+
+export type LoginInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type LoginOutput = {
+  __typename?: 'LoginOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  loginToken?: Maybe<Scalars['String']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createAccount: CreateAccountOutput;
+  createDish: CreateDishOutput;
+  createOrder: CreateOrderOutput;
+  createPayment: CreatePaymentOutput;
+  createRestaurant: CreateRestaurantOutput;
+  deleteDish: DeleteDishOutput;
+  deleteRestaurant: DeleteRestaurantOutput;
+  editDish: EditDishOutput;
+  editOrder: EditOrderOutput;
+  editProfile: EditProfileOutput;
+  editRestaurant: EditRestaurantOutput;
+  login: LoginOutput;
+  takeOrder: TakeOrderOutput;
+  verifyEmail: VerifyEmailOutput;
+};
+
+
+export type MutationCreateAccountArgs = {
+  input: CreateAccountInput;
+};
+
+
+export type MutationCreateDishArgs = {
+  input: CreateDishInput;
+};
+
+
+export type MutationCreateOrderArgs = {
+  input: CreateOrderInput;
+};
+
+
+export type MutationCreatePaymentArgs = {
+  input: CreatePaymentInput;
+};
+
+
+export type MutationCreateRestaurantArgs = {
+  input: CreateRestaurantInput;
+};
+
+
+export type MutationDeleteDishArgs = {
+  input: DeleteDishInput;
+};
+
+
+export type MutationDeleteRestaurantArgs = {
+  input: DeleteRestaurantInput;
+};
+
+
+export type MutationEditDishArgs = {
+  input: EditDishInput;
+};
+
+
+export type MutationEditOrderArgs = {
+  input: EditOrderInput;
+};
+
+
+export type MutationEditProfileArgs = {
+  input: EditProfileInput;
+};
+
+
+export type MutationEditRestaurantArgs = {
+  input: EditRestaurantInput;
+};
+
+
+export type MutationLoginArgs = {
+  input: LoginInput;
+};
+
+
+export type MutationTakeOrderArgs = {
+  input: TakeOrderInput;
+};
+
+
+export type MutationVerifyEmailArgs = {
+  input: VerifyEmailInput;
+};
+
+export type Order = {
+  __typename?: 'Order';
+  createdAt: Scalars['DateTime'];
+  customer?: Maybe<User>;
+  driver?: Maybe<User>;
+  id: Scalars['Float'];
+  items: Array<OrderItem>;
+  restaurant?: Maybe<Restaurant>;
+  status: OrderStatus;
+  total?: Maybe<Scalars['Float']>;
+  updatedAt: Scalars['DateTime'];
+};
+
+export type OrderItem = {
+  __typename?: 'OrderItem';
+  createdAt: Scalars['DateTime'];
+  dish: Dish;
+  id: Scalars['Float'];
+  options?: Maybe<Array<OrderItemOption>>;
+  updatedAt: Scalars['DateTime'];
+};
+
+export type OrderItemOption = {
+  __typename?: 'OrderItemOption';
+  choice: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type OrderItemOptionInputType = {
+  choice: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export enum OrderStatus {
+  Cooked = 'Cooked',
+  Cooking = 'Cooking',
+  Delivered = 'Delivered',
+  Pending = 'Pending',
+  PickedUp = 'PickedUp'
+}
+
+export type OrderUpdateInput = {
+  id: Scalars['Float'];
+};
+
+export type Payment = {
+  __typename?: 'Payment';
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Float'];
+  restaurant: Restaurant;
+  restaurantId: Scalars['Int'];
+  transactionId: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+  user: User;
+  userId: Scalars['Int'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allCategories: AllCategoriesOutput;
+  allRestaurants: AllRestaurantsOutput;
+  category: CategoryOutput;
+  findMe: User;
+  getOrder: GetOrderOutput;
+  getOrders: GetOrdersOutput;
+  getPayments: GetPaymentsOutput;
+  restaurant: RestaurantOutput;
+  searchRestaurant: SearchRestaurantOutput;
+  userProfile: UserProfileOutput;
+  users: Array<User>;
+};
+
+
+export type QueryAllRestaurantsArgs = {
+  input: AllRestaurantsInput;
+};
+
+
+export type QueryCategoryArgs = {
+  input: CategoryInput;
+};
+
+
+export type QueryGetOrderArgs = {
+  input: GetOrderInput;
+};
+
+
+export type QueryGetOrdersArgs = {
+  input: GetOrdersInput;
+};
+
+
+export type QueryRestaurantArgs = {
+  input: RestaurantInput;
+};
+
+
+export type QuerySearchRestaurantArgs = {
+  input: SearchRestaurantInput;
+};
+
+
+export type QueryUserProfileArgs = {
+  userId: Scalars['Float'];
+};
+
+export type Restaurant = {
+  __typename?: 'Restaurant';
+  address: Scalars['String'];
+  category?: Maybe<Category>;
+  coverImg: Scalars['String'];
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Float'];
+  isPromoted: Scalars['Boolean'];
+  menu: Array<Dish>;
+  name: Scalars['String'];
+  orders: Array<Order>;
+  owner: User;
+  promotedUntil?: Maybe<Scalars['DateTime']>;
+  updatedAt: Scalars['DateTime'];
+};
+
+export type RestaurantInput = {
+  restaurandId: Scalars['Int'];
+};
+
+export type RestaurantOutput = {
+  __typename?: 'RestaurantOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  restaurant?: Maybe<Restaurant>;
+};
+
+export type SearchRestaurantInput = {
+  page?: InputMaybe<Scalars['Int']>;
+  query: Scalars['String'];
+};
+
+export type SearchRestaurantOutput = {
+  __typename?: 'SearchRestaurantOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  restaurants?: Maybe<Array<Restaurant>>;
+  totalPages?: Maybe<Scalars['Int']>;
+  totalResults?: Maybe<Scalars['Int']>;
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  cookedOrders: Order;
+  orderUpdate: Order;
+  pendingOrders: Order;
+};
+
+
+export type SubscriptionOrderUpdateArgs = {
+  input: OrderUpdateInput;
+};
+
+export type TakeOrderInput = {
+  id: Scalars['Float'];
+};
+
+export type TakeOrderOutput = {
+  __typename?: 'TakeOrderOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type User = {
+  __typename?: 'User';
+  createdAt: Scalars['DateTime'];
+  email: Scalars['String'];
+  id: Scalars['Float'];
+  orders: Array<Order>;
+  password: Scalars['String'];
+  payments: Array<Payment>;
+  restaurants: Array<Restaurant>;
+  rides: Array<Order>;
+  role: UserRole;
+  updatedAt: Scalars['DateTime'];
+  verified: Scalars['Boolean'];
+};
+
+export type UserProfileOutput = {
+  __typename?: 'UserProfileOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+  user?: Maybe<User>;
+};
+
+export enum UserRole {
+  Client = 'Client',
+  Delivery = 'Delivery',
+  Owner = 'Owner'
+}
+
+export type VerifyEmailInput = {
+  code: Scalars['String'];
+};
+
+export type VerifyEmailOutput = {
+  __typename?: 'VerifyEmailOutput';
+  GraphQLError?: Maybe<Scalars['String']>;
+  GraphQLSucceed: Scalars['Boolean'];
+};
+
+export type LoginMutationVariables = Exact<{
+  loginInput: LoginInput;
+}>;
+
+
+export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'LoginOutput', GraphQLSucceed: boolean, GraphQLError?: string | null, loginToken?: string | null } };
+
+
+export const LoginDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"Login"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"loginInput"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"LoginInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"login"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"loginInput"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"GraphQLSucceed"}},{"kind":"Field","name":{"kind":"Name","value":"GraphQLError"}},{"kind":"Field","name":{"kind":"Name","value":"loginToken"}}]}}]}}]} as unknown as DocumentNode<LoginMutation, LoginMutationVariables>;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,81 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/client";
+import { FormError } from "../components/form-error"
+
+/* GraphQL Code Generator (Replaces Codegen): https://www.graphql-code-generator.com/docs/getting-started/installation */
+/* GraphQL Code Generator for React: https://www.graphql-code-generator.com/docs/guides/react#apollo-and-urql */
+/* - 'LoginMutation', 'LoginMutationVariables', and 'LoginDocument' are Created in 'generated.ts' from 'login.graphql' */
+/* - Don't Need to Declare a 'gql' Variable → Import 'LoginDocument' Instead */
+import { LoginDocument, LoginMutation, LoginMutationVariables } from "../graphql/generated";
+
+interface ILoginForm {
+    email: string;
+    password: string;
+    resultError?: string;
+}
 
 export const Login = () => {
-    return <span>Log In</span>
+    const { register, getValues, handleSubmit, formState: { errors } } = useForm<ILoginForm>();
+
+    const onCompleted = (data: LoginMutation) => {
+        const {
+            login: {
+                GraphQLSucceed,
+                GraphQLError,
+                loginToken
+            }
+        } = data;
+
+        if (GraphQLSucceed) {
+            console.log("------ Login ------ loginToken:", loginToken);
+        }
+    };
+
+    const [ loginMutation, { data: loginMutationResult } ] = useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, { onCompleted });
+    
+    const onSubmit = () => {
+        const { email, password } = getValues();
+
+        loginMutation({
+            variables: {
+                loginInput: {
+                    email,
+                    password
+                }
+            }
+        });
+    };
+
+    return (
+        <div className="h-screen flex items-center justify-center bg-gray-800">
+            <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+                <h3 className="text-2xl text-gray-800">Log In</h3>
+                <form
+                    className="grid gap-3 mt-5 px-5"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <input
+                        className="input"
+                        placeholder="Email"
+                        type="email"
+                        required
+                        {...register("email", { required: "Email is required" })}
+                    />
+                    {errors.email?.message && <FormError errorMessage={errors.email.message} />}
+                    <input
+                        className="input"
+                        placeholder="Password"
+                        type="password"
+                        required
+                        {...register("password", { required: "Password is required", minLength: 8 })}
+                    />
+                    {errors.password?.message && <FormError errorMessage={errors.password.message} />}
+                    {errors.password?.type === "minLength" && <FormError errorMessage="Password must be longer than 8 characters" />}
+                    <button className="btn mt-3">Log In</button>
+                    {loginMutationResult?.login.GraphQLError && <FormError errorMessage={loginMutationResult.login.GraphQLError} />}
+                </form>
+            </div>
+        </div>
+    );
 };
