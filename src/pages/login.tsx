ffcdbce1568da265c1c1f160bc3e6a354872dcb5
Younglong Kim <withel1013@gmail.com>
2022-08-03 06:59:48 +0900

    Button & CSS

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@apollo/client";
-import { FormError } from "../components/form-error"
+import { FormError } from "../components/form-error";
+import { Button } from "../components/button";
+import Logo from "../images/logo.svg";
 
 /* GraphQL Code Generator (Replaces Codegen): https://www.graphql-code-generator.com/docs/getting-started/installation */
 /* GraphQL Code Generator for React: https://www.graphql-code-generator.com/docs/guides/react#apollo-and-urql */
@@ -16,7 +19,9 @@ interface ILoginForm {
 }
 
 export const Login = () => {
-    const { register, getValues, handleSubmit, formState: { errors } } = useForm<ILoginForm>();
+    const { register, getValues, handleSubmit, formState } = useForm<ILoginForm>({
+        mode: "onChange"
+    });
 
     const onCompleted = (data: LoginMutation) => {
         const {
@@ -52,11 +57,12 @@ export const Login = () => {
     };
 
     return (
-        <div className="h-screen flex items-center justify-center bg-gray-800">
-            <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-                <h3 className="text-2xl text-gray-800">Log In</h3>
+        <div className="h-screen flex flex-col items-center mt-10 lg:mt-28">
+            <div className="w-full flex flex-col max-w-screen-sm px-5 items-center">
+                <img src={Logo} className="w-52 mb-10" />
+                <h4 className="w-full font-medium text-left text-3xl mb-5">Welcome Back</h4>
                 <form
-                    className="grid gap-3 mt-5 px-5"
+                    className="grid gap-3 mt-5 w-full mb-5"
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <input
@@ -66,7 +72,7 @@ export const Login = () => {
                         required
                         {...register("email", { required: "Email is required" })}
                     />
-                    {errors.email?.message && <FormError errorMessage={errors.email.message} />}
+                    {formState.errors.email?.message && <FormError errorMessage={formState.errors.email.message} />}
                     <input
                         className="input"
                         placeholder="Password"
@@ -74,11 +80,17 @@ export const Login = () => {
                         required
                         {...register("password", { required: "Password is required", minLength: 8 })}
                     />
-                    {errors.password?.message && <FormError errorMessage={errors.password.message} />}
-                    {errors.password?.type === "minLength" && <FormError errorMessage="Password must be longer than 8 characters" />}
-                    <button className="btn mt-3">{loading ? "Loading..." : "Log In"}</button>
+                    {formState.errors.password?.message && <FormError errorMessage={formState.errors.password.message} />}
+                    {formState.errors.password?.type === "minLength" && <FormError errorMessage="Password must be longer than 8 characters" />}
+                    <Button canClick={formState.isValid} loading={loading} actionText={"Log In"} />
                     {loginMutationResult?.login.GraphQLError && <FormError errorMessage={loginMutationResult.login.GraphQLError} />}
                 </form>
+                <div>
+                    New to Uber?{" "}
+                    <Link to="/create-account" className="text-lime-600 hover:underline">
+                        Create an Account
+                    </Link>
+                </div>
             </div>
         </div>
     );
